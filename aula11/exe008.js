f92b8072178d555a51f74e4bb3fef6e4e030d904
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-04 05:41:33 -0300

    condições simples

---
+++ aula11/exe008.js
@@ -0,0 +1,7 @@
+var vel = 60.0
+
+console.log(`sua velocidade é de ${vel} Kh`)
+if(vel>60){
+    console.log(`Você passou a velocidade permitida, MULTADO!`)
+}
+console.log('Diriga sempre com segurança , use cinto!')
