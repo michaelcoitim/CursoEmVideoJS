473e6bbcce11c218312475375c503ed7ef093e5f
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-04 05:51:42 -0300

    condições Conposta

--- aula11/exe008.js
+++ aula11/exe008.js
@@ -1,6 +1,6 @@
 var vel = 60.0
 
-console.log(`sua velocidade é de ${vel} Kh`)
+console.log(`sua velocidade é de ${vel} Kh`)//condição simples 
 if(vel>60){
     console.log(`Você passou a velocidade permitida, MULTADO!`)
 }
---
+++ aula11/exx009.js
@@ -0,0 +1,7 @@
+var país="França" //no Js é Permitido acento nas variaveis 
+
+if(país =="Brasil"){
+    console.log("você é Brasileiro!")
+}else{
+    console.log("você é Estrangeiro")
+}
