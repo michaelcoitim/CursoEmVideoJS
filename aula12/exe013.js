fe13e5790e5c77a5e83cfcb172392c29dc3d33c4
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-05 07:25:15 -0300

    Dias da semana

---
+++ aula12/exe013.js
@@ -0,0 +1,39 @@
+/** /dias da semanas */
+
+var agora = new Date()
+var diaSem= agora.getDay() 
+/* dias da semana JS
+    .getDay busca o dias da semana 
+    0- Domingo
+    1- Segunda 
+    2- Terça 
+    3- Quarta 
+    4- Quinta
+    7- Sexta
+    6- Sabado 
+ */
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexxxtou, Sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('[ERRO] Dia invalido!')
+}
