ea26972f697390d0f148a52a3384ae7cfb320f01
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-08 07:01:56 -0300

    adicionando fotos no codigo

--- exe014/script.js
+++ exe014/script.js
@@ -2,17 +2,19 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-
+    ///var hora = data.getHours()
+var hora=1
     msg.innerHTML =`Agora são ${hora} horas`
 
     if(hora >=0 && hora <12){
         //bom dia 
-
+        img.src= 'manha.png'
     }else if (hora  >=12 && hora <18){
         // boa tarde
+        img.src='tarde.png'
     } else {
         // boa noite
+        img.src='noite.png'
     }
 
 
