c4ac0fb12ce52c6b000b0e831f9ab2d15e3cc3d6
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-08 07:36:18 -0300

    adicionando Saudação

--- exe014/script.js
+++ exe014/script.js
@@ -2,19 +2,26 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    ///var hora = data.getHours()
-var hora=1
-    msg.innerHTML =`Agora são ${hora} horas`
+    //var hora = data.getHours()
+    var hora=5
+    
+    msg.innerHTML =`Agora são ${hora} horas,`
 
-    if(hora >=0 && hora <12){
+    if(hora >=0 && hora < 12){
         //bom dia 
         img.src= 'manha.png'
-    }else if (hora  >=12 && hora <18){
+        document.body.style.background='#b5625e'
+        msg.innerHTML +='<br/> Bom dia'
+    }else if (hora  >= 12 && hora <= 18){
         // boa tarde
+        document.body.style.background='#c54d01'
         img.src='tarde.png'
+        msg.innerHTML +='<br/> Boa Tarde'
     } else {
         // boa noite
         img.src='noite.png'
+        document.body.style.background='#353232'
+        msg.innerHTML +='<br/>Bao Noite'
     }
 
 
