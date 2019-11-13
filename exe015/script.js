41d59d22f8e3f6683762c5294e87fcbb6577d93d
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-13 05:48:37 -0300

    teste idade

--- exe015/script.js
+++ exe015/script.js
@@ -1,3 +1,12 @@
 function verificar(){
-    window.alert('Teste')
-}
+    var date = new Date()
+    var ano = date.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if(fano.value.length == 0 || Number(fano.value) > ano || Number(fano.value<0)){
+         window.alert('Verifique os dados e tente novamente')
+    }else
+        var fsex = document.getElementsByName('sexo')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `sua idade é ${idade}, sexo = ${fsex.value}`
+    }
