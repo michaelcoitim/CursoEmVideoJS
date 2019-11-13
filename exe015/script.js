6e654cbb7c97234c3d36a4e00ba6af5f0721179e
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-13 06:57:35 -0300

    add img com js

--- exe015/script.js
+++ exe015/script.js
@@ -8,5 +8,49 @@ function verificar(){
     }else
         var fsex = document.getElementsByName('sexo')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `sua idade é ${idade}, sexo = ${fsex.value}`
+        var genero= ' '
+        var img = document.createElement('img')
+        img.setAttribute('id','foto') //criando foto dinamicamente com js
+
+        if(fsex[0].checked){  //buscando elementos radio (sexo selecionando)
+            genero='Homem'
+            if(idade >=0 && idade <10){
+                // crianca
+                img.setAttribute('src', 'menino.png')
+            }else if(idade <21){
+                // jovem
+                img.setAttribute('src', 'adlmenino.png')
+
+            }else if (idade <50){
+                //adulto
+                img.setAttribute('src','homenadulto.png')
+            }else{
+                // idoso
+                img.setAttribute('src','idoso.png')
+            }
+        }else{
+            genero='Mulher'
+            if(idade >=0 && idade <10){
+                // crianca
+                img.setAttribute('src','menina.png')
+
+            }else if(idade <21){
+                // jovem
+                img.setAttribute('src','adlmenina.png')
+
+            }else if (idade < 50){
+                //adulta
+                img.setAttribute('src','mulheradulta.png')
+            }else{
+                // idosa
+                img.setAttribute('src','idosa.png')
+            }
+        }
+    
+
+    
+        res.style.textAlign='center' //ajustando o css pelo JS
+       // res.innerHTML=`${img.src} <br/>` não funcionou 
+        res.innerHTML+=`${genero} idade ${idade}`
+        res.appendChild(img)
     }
