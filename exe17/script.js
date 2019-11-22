d4415e91aa015366354c0e71d9dfa698c6d71545
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-22 05:31:24 -0300

    tabuada sem sec

--- exe17/script.js
+++ exe17/script.js
@@ -1,9 +1,9 @@
 function Tabuada(){
     let n = window.document.getElementById('txtn')
     let res = window.document.getElementById('res')
-
-    for(let c = 1; c > 10; c++){
+    res.innerHTML='calculando <br/> '
+    for(let c = 0; c <= 10; c++){
         let tab= Number(n.value) * c
-        res.innerHTML=`${Number(n.value)} * ${c} = ${tab} <br>`
+        res.innerHTML+=`${Number(n.value)} * ${c} = ${tab} <br>`
     }
 }
