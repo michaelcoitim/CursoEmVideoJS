6e0e28758c636226d2063403c90278f92029d6f5
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-25 06:37:55 -0300

    guardando uma função em uma variável

--- aula16/funcao02.js
+++ aula16/funcao02.js
@@ -1,5 +1,5 @@
-function soma (n1 , n2){
+function soma (n1=0 , n2=0){ //paremetros se não for informado, irá ser considerado o parâmetro pre-definido
     return n1 + n2 
 }
 
-console.log( soma(5,7))
+console.log( soma(5))
---
+++ aula16/funcao03.js
@@ -0,0 +1,7 @@
+// passando uma função para uma variável
+
+let v = function (x){
+    return x *2
+}
+
+console.log(v(5))// mostrando o conteudo da  variável, já passando o parâmetro e chamando a função anonima
