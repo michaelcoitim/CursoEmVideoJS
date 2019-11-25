f30297864446b4ea4b15cb30901727d84d4d5e84
Michael Robert Coitim <michaelcoitim1@gmail.com>
2019-11-25 06:24:07 -0300

    função soma

--- aula16/funcao01.js
+++ aula16/funcao01.js
@@ -8,6 +8,8 @@ function parimpar (n){
     }
 }
 
-let res = parimpar(5) // camada da função passando parâmetro , e guardando seu valor na vareavel
+let res = parimpar(4) // camada da função passando parâmetro , e guardando seu valor na vareavel
 
 console.log(res) // mostra o resultado salvo na vareavel res 
+
+console.log(parimpar(479))// outro modo de mostrar o resultado 
